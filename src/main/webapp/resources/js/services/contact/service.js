e0063d2d0b51b67060f6b78feeea963625d7ab73
hassench <charef.hassen@gmail.com>
2016-11-04 13:42:46 +0100

    Initial commit

---
+++ src/main/webapp/resources/js/services/contact/service.js
@@ -0,0 +1,16 @@
+'use strict';
+ 
+App.factory('ContactService', ['$http', '$q', function($http, $q){
+ 
+    return {
+            send: function(name, subject, email, msg) {
+                    return $http.get('http://localhost:8080/project/service/contact?name='+name+'&email='+email+"&subject="+subject+"&msg="+msg)
+                            .then(
+                                    function(response){
+                                        return response.data;
+                                    }
+                            );
+            }
+    };
+ 
+}]);
